218f51f7fa4ef07163196a1e4b98f797c962e6a1
kingl <kingl@DESKTOP-DHOEEHJ>
2023-11-17 11:26:38 +0500

    firstt

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,15 +24,5 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
